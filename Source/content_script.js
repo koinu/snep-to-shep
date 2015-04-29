efbcba35b5b1adb69dd662829dfadd35d26d17fd
kieran <kieran@snowme.ws>
2015-04-29 12:56:10 -0400

    add plural variations

--- Source/content_script.js
+++ Source/content_script.js
@@ -34,6 +34,8 @@ function handleText(textNode)
 	v = v.replace(/\bshep\b/g, "snep");
 	v = v.replace(/\bShep\b/g, "Snep");
 	v = v.replace(/\bSHEP\b/g, "SNEP");
+	v = v.replace(/\bsheps\b/g, "sneps");
+	v = v.replace(/\bSheps\b/g, "Sneps");
 	v = v.replace(/\bsheppy\b/g, "sneppy");
 	v = v.replace(/\bSheppy\b/g, "Sneppy");
 	v = v.replace(/\bshepherd\b/g, "snepherd");
